f7e8882bf114866f6277ed714fffded30da2f63b
adriaanbakker-java <adriaan.bakker@hotmail.com>
2020-09-21 19:09:30 +0200

    excel portf bijgewerkt, begin gemaakt met webapp voor beleggingspakket

---
+++ Webapplicatie/beleggingspakket/src/main/java/ideaalbeleggen/beleggingspakket/BeleggingspakketApplication.java
@@ -0,0 +1,13 @@
+package ideaalbeleggen.beleggingspakket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeleggingspakketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeleggingspakketApplication.class, args);
+	}
+
+}
