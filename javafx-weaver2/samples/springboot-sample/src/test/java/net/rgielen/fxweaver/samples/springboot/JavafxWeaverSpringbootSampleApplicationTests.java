12e9d52a619a859827992471deb486cd5f40831a
adriaanbakker-java <55943332+adriaanbakker-java@users.noreply.github.com>
2020-09-25 13:14:57 +0200

    javafx-weaver added as javafx-weaver2 with renamed git repo

---
+++ javafx-weaver2/samples/springboot-sample/src/test/java/net/rgielen/fxweaver/samples/springboot/JavafxWeaverSpringbootSampleApplicationTests.java
@@ -0,0 +1,14 @@
+package net.rgielen.fxweaver.samples.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafxWeaverSpringbootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	    // do nothing
+	}
+
+}
