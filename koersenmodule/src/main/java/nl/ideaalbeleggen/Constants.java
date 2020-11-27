4a2041a5f7159f1fd183cfc2fc262e720667babb
adriaanbakker-java <55943332+adriaanbakker-java@users.noreply.github.com>
2020-11-27 14:58:42 +0100

    koersen ververst, koersenmodule constants zoekt zelf paden uit

--- koersenmodule/src/main/java/nl/ideaalbeleggen/Constants.java
+++ koersenmodule/src/main/java/nl/ideaalbeleggen/Constants.java
@@ -1,26 +1,59 @@
 package nl.ideaalbeleggen;
 
+import java.io.File;
+
 public class Constants {
 
+    private static String folderStockprices = "stockprices"  ;
+    private static String folderPortfolio = "portfolio";
+
     // BKWI laptop
-    private static String PricefolderMACBKWI = "/Users/abakker/Pakket/ideaal-beleggen/stockprices/";
+    private static String folderBKWI = "/Users/abakker/Pakket/ideaal-beleggen/";
+    private static String PricefolderMACBKWI =  folderBKWI + folderStockprices + "/";
+    private static String PFfolderBKWI =  folderBKWI + folderPortfolio + "/";
 
     // Ordina laptop
-    private static String PricefolderMACOrdina = "/Users/aba23913/Pakket/ideaal-beleggen/stockprices/";
+    private static String folderOrdina =  "/Users/aba23913/Pakket/ideaal-beleggen/";
+    private static String PricefolderMACOrdina = folderOrdina + folderStockprices  + "/";
+    private static String PFfolderMacOrdina = folderOrdina  + folderPortfolio  + "/";
+
+    // Windows pc
+    private static final String folderPC = "D:\\Beleggingspakket\\ideaal-beleggen\\";
+    private static final String PricefolderPC = folderPC  + folderStockprices + "\\";
+    private static final String PFfolderPC = folderPC  + folderPortfolio + "\\";
 
 
-    private static String PricefolderPC = "D:\\Beleggingspakket\\ideaal-beleggen\\stockprices\\";
-        //"d:\\stockprices\\";
 
-    public static void setPricefolder(String pricefolder) {
-        Pricefolder = pricefolder;
+    private static final int CEigenPC = 0;
+    private static final int COrdinaPC = CEigenPC + 1;
+    private static final int CBKWIPC = COrdinaPC + 1;
+
+    private static int geefPCId() {
+        File myFile = new File(folderPC);
+        if (myFile.exists()) {
+            return CEigenPC;
+        }
+        myFile = new File(folderBKWI);
+        if (myFile.exists()) {
+            return CBKWIPC;
+        }
+        myFile = new File(folderOrdina);
+        if (myFile.exists()) {
+            return COrdinaPC;
+        }
+
+        return -1;
     }
 
-    private static String Pricefolder = PricefolderMACBKWI;
-
-
-    public static String getPricefolder() {
-        return Pricefolder;
+    public static String getPricefolder() throws Exception {
+        int pcId = geefPCId();
+        switch (pcId) {
+            case CEigenPC: return PricefolderPC;
+            case COrdinaPC: return PricefolderMACOrdina;
+            case CBKWIPC: return PricefolderMACBKWI;
+            default:
+                throw new IllegalStateException("getPricefolder: kon PC folders niet bepalen");
+        }
     }
 
     public static final int startYear = 2014;
